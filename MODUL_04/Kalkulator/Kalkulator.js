6cf8c242b1f0ccaa0b9c264f526d264e1ffb99b3
Ahmad Firdaus Sulintang <20230910087@uniku.ac.id>
2025-01-06 21:31:59 +0700

    Add files via upload

---
+++ MODUL_04/Kalkulator/Kalkulator.js
@@ -0,0 +1,30 @@
+var bil1 = "", bil2 = "", bil3 = "", opr = "";
+function Angka(Angka) {
+    document.getElementById('hasil').value += Angka;
+}
+
+function Operator(o) {
+    bil1 = document.getElementById('hasil').value;
+    document.getElementById('hasil').value = "";
+    opr = o;
+}
+function Hasil() {
+    bil2 = document.getElementById('hasil').value;
+    switch (opr) {
+        case '+':
+            bil3 = parseFloat(bil1) + parseFloat(bil2);
+            break;
+
+        case '-':
+            bil3 = parseFloat(bil1) - parseFloat(bil2);
+            break;
+        case '*':
+            bil3 = parseFloat(bil1) * parseFloat(bil2);
+            break;
+
+        case '/':
+            bil3 = parseFloat(bil1) / parseFloat(bil2);
+            break;
+    }
+    document.getElementById('hasil').value = bil3;
+}
